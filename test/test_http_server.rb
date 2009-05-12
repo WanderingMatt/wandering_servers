eb7bc60887c641ba958bf5b8a83ad02110aab7d9
Matthew Anderson <manderson@onehub.com>
2009-05-12 16:29:49 -0700

    Fleshing out response tests and adding a test for parsing the file from the request

--- test/test_http_server.rb
+++ test/test_http_server.rb
@@ -1,10 +1,21 @@
 require 'test/unit'
 require 'http_server'
+require 'fileutils'
 
-class TestHTTPServer < Test::Unit::TestCase
+class TestHttpServer < Test::Unit::TestCase
   def setup
-    test_io = 'Hello World!'
-    @test_request = Reqeust.new test_io
+    @test_path = 'test.html'
+    FileUtils.touch @test_path
+    test_io = 'GET /#{@test_path} HTTP/1.0\r\n\r\n'
+    @test_request = HttpServer::Request.new test_io
+  end
+  
+  def teardown
+    FileUtils.rm @test_path if File.exists? @test_path
+  end
+  
+  def test_parses_file_from_request
+    assert_equal '/test.html', @test_request.path
   end
   
   def test_responds_with_200_when_successful
@@ -12,8 +23,8 @@ class TestHTTPServer < Test::Unit::TestCase
   end
   
   def test_responds_with_404_when_missing
-    actual = Request.new 'flunk'
+    actual = HttpServer::Request.new 'GET /flunk.html HTTP/1.0\r\n\r\n'
     
-    assert_match /404 Not Found/, actual
+    assert_match /404 Not Found/, actual.header
   end
 end
