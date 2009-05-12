808c3bbee63532c4ff458f6237c84900b7d05407
Matthew Anderson <manderson@onehub.com>
2009-05-12 15:33:45 -0700

    Initial tests for basic server response

--- test/test_http_server.rb
+++ test/test_http_server.rb
@@ -2,4 +2,18 @@ require 'test/unit'
 require 'http_server'
 
 class TestHTTPServer < Test::Unit::TestCase
+  def setup
+    test_io = 'Hello World!'
+    @test_request = Reqeust.new test_io
+  end
+  
+  def test_responds_with_200_when_successful
+    assert_match /200 OK/, @test_request.header
+  end
+  
+  def test_responds_with_404_when_missing
+    actual = Request.new 'flunk'
+    
+    assert_match /404 Not Found/, actual
+  end
 end
