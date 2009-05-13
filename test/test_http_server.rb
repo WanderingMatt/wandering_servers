666a680d9c5cbea19f5394891f8b23bf1120f40b
Matthew Anderson <manderson@onehub.com>
2009-05-12 17:40:04 -0700

    Creating StringIOs in my tests and fixing a naming glitch

--- test/test_http_server.rb
+++ test/test_http_server.rb
@@ -1,17 +1,22 @@
 require 'test/unit'
 require 'http_server'
 require 'fileutils'
+require 'stringio'
 
 class TestHttpServer < Test::Unit::TestCase
   def setup
     @test_path = 'test.html'
     FileUtils.touch @test_path
-    test_io = 'GET /#{@test_path} HTTP/1.0\r\n\r\n'
+    test_io = StringIO.new "GET /#{@test_path} HTTP/1.0\r\n\r\n"
     @test_request = HttpServer::Request.new test_io
   end
   
   def teardown
     FileUtils.rm @test_path if File.exists? @test_path
+  end
+  
+  def test_parses_method_from_request
+    assert_equal 'GET', @test_request.method
   end
   
   def test_parses_file_from_request
@@ -23,7 +28,7 @@ class TestHttpServer < Test::Unit::TestCase
   end
   
   def test_responds_with_404_when_missing
-    actual = HttpServer::Request.new 'GET /flunk.html HTTP/1.0\r\n\r\n'
+    actual = HttpServer::Request.new StringIO.new 'GET /flunk.html HTTP/1.0\r\n\r\n'
     
     assert_match /404 Not Found/, actual.header
   end
