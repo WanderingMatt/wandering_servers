b06d2ac0a41ba51c71e05d2b32dbf650facf3b4b
Matthew Anderson <manderson@onehub.com>
2009-05-13 19:56:55 -0700

    Pruned some request tests

--- test/test_request.rb
+++ test/test_request.rb
@@ -2,20 +2,8 @@ require 'helper'
 
 class TestRequest < Test::Unit::TestCase
   def setup
-    test_io = StringIO.new "GET /#{File.basename(__FILE__)} HTTP/1.0\r\n\r\n"
+    test_io = StringIO.new "GET /#{File.basename(__FILE__)} HTTP/1.0\r\nHost: flunk.com\r\nDate: today\r\n\r\nBody!"
     @test_request = HttpServer::Request.new test_io
-  end
-  
-  def test_read_stops_after_double_crlf
-    test_io = StringIO.new "GET /#{File.basename(__FILE__)} HTTP/1.0\r\nHost: foo\r\nDate: today\r\n\r\nhello!"
-    test_request = HttpServer::Request.new test_io
-    assert_equal 'hello!', test_io.read
-  end
-  
-  def test_bogus_headers_are_ignored
-    test_io = StringIO.new "GET /#{File.basename(__FILE__)} HTTP/1.0\r\nHost: foo\r\nDate: today\r\n\r\n"
-    test_request = HttpServer::Request.new test_io
-    assert_equal "/#{File.basename(__FILE__)}", @test_request.path
   end
   
   def test_parses_method_from_request
@@ -24,5 +12,9 @@ class TestRequest < Test::Unit::TestCase
   
   def test_parses_file_from_request
     assert_equal "/#{File.basename(__FILE__)}", @test_request.path
+  end
+  
+  def test_read_stops_after_double_crlf
+    assert_equal 'Body!', test_io.read
   end
 end
