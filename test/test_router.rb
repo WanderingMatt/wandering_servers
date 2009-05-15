51071ae830aa890c464b3be856fe0134fbd2235f
Matthew Anderson <manderson@onehub.com>
2009-05-14 19:59:19 -0700

    Failing tests for content types

--- test/test_router.rb
+++ test/test_router.rb
@@ -26,27 +26,7 @@ class TestRouter < Test::Unit::TestCase
     response = io.string.split("\r\n")
     assert response.include?('HTTP/0.9 500 Internal Server Error'), 'should have 404 status'
     assert response.include?('Content-Type: text/html'), "should have content type header"
-    assert response.include?('<html><body><h1>500 Internal Server Error</h1></body></html>')
-  end
-  
-  def test_responds_with_directories
-    io = StringIO.new("GET /test/ HTTP/1.0\r\n\r\n")
-    @router.accept(io)
-    
-    response = io.string.split("\r\n")
-    assert response.include?('HTTP/0.9 200 OK'), 'should have 200 status'
-    assert response.include?('Content-Type: text/html'), "should have content type header"
-    assert_match "#{__FILE__}", response.last
-  end
-  
-  def test_responds_with_files
-    io = StringIO.new("GET /test/#{File.basename(__FILE__)} HTTP/1.0\r\n\r\n")
-    @router.accept(io)
-
-    response = io.string.split("\r\n")
-    assert response.include?('HTTP/0.9 200 OK'), 'should have 200 status'
-    assert response.include?('Content-Type: text/html'), "should have content type header"
-    assert response.include?(File.read(__FILE__)), "is this broken"
+    assert_match '500 Internal Server Error', response.last
   end
   
   def test_routes_a_proc
@@ -80,5 +60,49 @@ class TestRouter < Test::Unit::TestCase
     response = io.string.split("\r\n")
     assert response.include?('Content-Type: text/html'), "should have content type header"
     assert response.include?(body)
+  end
+  
+  def test_responds_with_directories
+    io = StringIO.new("GET /test/ HTTP/1.0\r\n\r\n")
+    @router.accept(io)
+    
+    response = io.string.split("\r\n")
+    assert response.include?('HTTP/0.9 200 OK'), 'should have 200 status'
+    assert response.include?('Content-Type: text/html'), "should have content type header"
+    assert_match "#{File.basename(__FILE__)}", response.last
+  end
+  
+  def test_responds_with_files
+    io = StringIO.new("GET /test/#{File.basename(__FILE__)} HTTP/1.0\r\n\r\n")
+    @router.accept(io)
+
+    response = io.string.split("\r\n")
+    assert response.include?('HTTP/0.9 200 OK'), 'should have 200 status'
+    assert response.include?('Content-Type: text/html'), "should have content type header"
+    assert response.include?(File.read(__FILE__)), "is this broken"
+  end
+  
+  def test_html_content_type
+    actual = @router.get_content_type "test.html"
+    expected = "text/html"
+    assert_equal expected, actual
+  end
+ 
+  def test_erb_content_type
+    actual = @router.get_content_type "test.html.erb"
+    expected = "text/html"
+    assert_equal expected, actual
+  end
+ 
+  def test_rb_content_type
+    actual = @router.get_content_type "test.rb"
+    expected = "text/plain"
+    assert_equal expected, actual
+  end
+ 
+  def test_missing_content_type
+    actual = @router.get_content_type "noext"
+    expected = "text/plain"
+    assert_equal expected, actual
   end
 end
