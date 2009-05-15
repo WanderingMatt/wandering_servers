b7ee48fb668192e24fef172f8db47e6d824280d4
Matthew Anderson <manderson@onehub.com>
2009-05-14 19:58:47 -0700

    Directory listing works

--- lib/http_server/router.rb
+++ lib/http_server/router.rb
@@ -10,27 +10,57 @@ module HttpServer
     def accept io
       request = Request.new io
       response = Response.new
-      
-      if SERVLETS.key? request.path
-        SERVLETS[request.path].call request, response
+      begin
+        if SERVLETS.key? request.path
+          SERVLETS[request.path].call request, response
+          response.write_to io
+        else
+          serve(request.path, response).write_to io
+        end
+      rescue => detail
+        response.status = 500
+        response.headers['Content-Type'] = 'text/html'
+        response.body = "<html><body><h1>500 Internal Server Error</h1><h2>#{detail.to_s}</h2></body></html>"
         response.write_to io
-        
-      else
-        serve_file(request.path, response).write_to io
       end
     end
     
-    def serve_file path, response
-      file = ROOT + path
-      if File.exists? file
-        response.status = 200
-        response.headers['Content-Type'] = 'text/html'
-        response.body = File.read(file)
+    def serve path, response
+      lookup = ROOT + path
+      if File.directory? lookup
+        serve_directory lookup, response
+      elsif File.exists? lookup
+        serve_file lookup, response
       else
-        response.status = 404
-        response.headers['Content-Type'] = 'text/html'
-        response.body = "<html><body><h1>404: File Not Found</h1></body></html>"
+        serve_404 path, response
       end
+      response
+    end
+    
+    def serve_directory dir, response
+      response.status = 200
+      response.headers['Content-Type'] = 'text/html'
+      items = Dir.new(dir).entries.reject{ |f| f =~ /^\./ }
+       response.body = "<html><head></head><body><ul style=\"list-style-type:none;margin:0;padding:0;\">"
+       items.each do |item|
+         response.body << "<li>"
+         response.body += File.directory?(item) ? "<strong>#{item}/</strong>" : "#{item}"
+         response.body += "</li>"
+       end
+       response.body += "</ul></body></html>"
+     end
+    
+    def serve_file file, response
+      response.status = 200
+      response.headers['Content-Type'] = 'text/html'
+      response.body = File.read(file)
+      response
+    end
+    
+    def serve_404 path, response
+      response.status = 404
+      response.headers['Content-Type'] = 'text/html'
+      response.body = "<html><body><h1>404: File Not Found</h1></body></html>"
       response
     end
   end
