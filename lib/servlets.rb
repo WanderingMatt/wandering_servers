9f9d64e06517d94d0e67df41736d1620e4f01123
Matthew Anderson <manderson@onehub.com>
2009-05-15 00:05:04 -0700

    Handled via tests

--- lib/servlets.rb
+++ lib/servlets.rb
@@ -1,5 +1,4 @@
 HttpServer::Router.register '/time' do |request, response|
-  called = true
   response.status = 200
   response.headers['Content-Type'] = 'text/html'
   response.body = "<html><body><h1>#{Time.now}</h1></body></html>"
@@ -8,7 +7,6 @@ end
 # REDIRECTS
 
 HttpServer::Router.register '/date' do |request, response|
-  called = true
   response.status = 301
   response.headers['Location'] = '/time'
 end
