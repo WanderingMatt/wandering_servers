ca7b327450a7b1ecf2d8d9280639b1305aaaa5a2
Matthew Anderson <manderson@onehub.com>
2009-05-14 20:35:50 -0700

    Testing that .erb gets rendered and cleanup

--- test/test_router.rb
+++ test/test_router.rb
@@ -10,9 +10,9 @@ class TestRouter < Test::Unit::TestCase
     @router.accept(io)
 
     response = io.string.split("\r\n")
-    assert response.include?('HTTP/0.9 404 Not Found'), 'should have 404 status'
-    assert response.include?('Content-Type: text/html'), "should have content type header"
-    assert response.include?('<html><body><h1>404: File Not Found</h1></body></html>')    
+    assert response.include?('HTTP/0.9 404 Not Found'), 'Should have 404 status'
+    assert response.include?('Content-Type: text/html'), 'Should have a Content-Type header'
+    assert_match '404 File Not Found', response.last, 'Should have a 400 in the body'
   end
   
   def test_500
@@ -24,9 +24,19 @@ class TestRouter < Test::Unit::TestCase
     @router.accept(io)
 
     response = io.string.split("\r\n")
-    assert response.include?('HTTP/0.9 500 Internal Server Error'), 'should have 404 status'
-    assert response.include?('Content-Type: text/html'), "should have content type header"
-    assert_match '500 Internal Server Error', response.last
+    assert response.include?('HTTP/0.9 500 Internal Server Error'), 'Should have 404 status'
+    assert response.include?('Content-Type: text/html'), 'Should have a Content-Type header'
+    assert_match '500 Internal Server Error', response.last, 'Should have a 500 in the body'
+  end
+  
+  def test_301
+    io = StringIO.new("GET /date HTTP/1.0\r\n\r\n")
+    @router.accept(io)
+
+    response = io.string.split("\r\n")
+    assert response.include?('HTTP/0.9 301 Moved Permanently'), 'Should have 301 status'
+    assert response.include?('Content-Type: text/html'), 'Should have a Content-Type header'
+    assert response.include?('Location: /time'), 'Should have a Location header'
   end
   
   def test_routes_a_proc
@@ -39,27 +49,27 @@ class TestRouter < Test::Unit::TestCase
     io = StringIO.new("GET /time HTTP/1.0\r\n\r\n")
     @router.accept(io)
     
-    assert called, "our proc should have been called"
+    assert called, 'Our proc should have been called'
   end
   
   def test_routes_a_proc_and_renders_body
     called = false
-    body = "<html><body><h1>#{Time.now}</h1></body></html>"
     HttpServer::Router.register '/time' do |request, response|
       called = true
       response.status = 200
       response.headers['Content-Type'] = 'text/html'
-      response.body = body
+      response.body = "<html><body><h1>#{Time.now}</h1></body></html>"
     end
     
     io = StringIO.new("GET /time HTTP/1.0\r\n\r\n")
     @router.accept(io)
     
-    assert called, "our proc should have been called"
+    assert called, 'Our proc should have been called'
 
     response = io.string.split("\r\n")
-    assert response.include?('Content-Type: text/html'), "should have content type header"
-    assert response.include?(body)
+    assert response.include?('Content-Type: text/html'), 'Should have a Content-Type header'
+    assert_match "#{Time.now}", response.last, 'Should have the time in the body'
+    
   end
   
   def test_responds_with_directories
@@ -67,9 +77,9 @@ class TestRouter < Test::Unit::TestCase
     @router.accept(io)
     
     response = io.string.split("\r\n")
-    assert response.include?('HTTP/0.9 200 OK'), 'should have 200 status'
-    assert response.include?('Content-Type: text/html'), "should have content type header"
-    assert_match "#{File.basename(__FILE__)}", response.last
+    assert response.include?('HTTP/0.9 200 OK'), 'Should have 200 status'
+    assert response.include?('Content-Type: text/html'), 'Should have a Content-Type header'
+    assert_match "#{File.basename(__FILE__)}", response.last, 'Should list this file in the body'
   end
   
   def test_responds_with_files
@@ -77,32 +87,45 @@ class TestRouter < Test::Unit::TestCase
     @router.accept(io)
 
     response = io.string.split("\r\n")
-    assert response.include?('HTTP/0.9 200 OK'), 'should have 200 status'
-    assert response.include?('Content-Type: text/html'), "should have content type header"
-    assert response.include?(File.read(__FILE__)), "is this broken"
+    assert response.include?('HTTP/0.9 200 OK'), 'Should have 200 status'
+    assert response.include?('Content-Type: text/html'), 'Should have a Content-Type header'
+    assert response.include?(File.read(__FILE__)), 'Should return this file in the body'
   end
   
   def test_html_content_type
-    actual = @router.get_content_type "test.html"
+    actual = @router.content_type "test.html"
     expected = "text/html"
     assert_equal expected, actual
   end
  
   def test_erb_content_type
-    actual = @router.get_content_type "test.html.erb"
+    actual = @router.content_type "test.html.erb"
     expected = "text/html"
     assert_equal expected, actual
   end
  
   def test_rb_content_type
-    actual = @router.get_content_type "test.rb"
+    actual = @router.content_type "test.rb"
     expected = "text/plain"
     assert_equal expected, actual
   end
  
   def test_missing_content_type
-    actual = @router.get_content_type "noext"
+    actual = @router.content_type "noext"
     expected = "text/plain"
     assert_equal expected, actual
+  end
+ 
+  def test_erb_gets_rendered
+    io = StringIO.new("GET /test/test.html.erb HTTP/1.0\r\n\r\n")
+    @router.accept(io)
+ 
+    response = io.string.split("\r\n")
+    erb = ERB.new(File.read('./test/test.html.erb')).src
+ 
+    assert response.include?('HTTP/0.9 200 OK'), 'should have 200 status code'
+    assert response.include?('Content-Type: text/html'), "should have content type header"
+    assert response.include?(eval(erb,binding)), "has the html-ified version of the ERB file"
+    assert_match(/This\sis\snot\sa\srandom\svariable/, response.last)
   end
 end
