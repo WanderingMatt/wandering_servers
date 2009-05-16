f48099e8ffd29cf3a9f31d4e84e0f812b65a82dd
Matthew Anderson <manderson@onehub.com>
2009-05-15 19:20:25 -0700

    TCPServer threading?

--- HTTPServer/lib/wandering_tcp_server.rb
+++ HTTPServer/lib/wandering_tcp_server.rb
@@ -1,11 +1,18 @@
 require 'http_server'
 require 'socket'
 
+THREAD_LIMIT = 5
+
 server = TCPServer.new 8080
 router = HttpServer::Router.new
+thread_pool = []
 while session = server.accept
-  Thread.new session do |my_session|
-    router.accept my_session
-    my_session.close
+  if thread_pool.size < THREAD_LIMIT
+    thread_pool << Thread.new(session) do |my_session|
+      router.accept my_session
+      my_session.close
+      # sleep 3
+      thread_pool.delete Thread.current
+    end
   end
 end
